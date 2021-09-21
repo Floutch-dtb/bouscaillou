fefb7dc8fecffee2af5328c96448133cde06860b
Floutch-dtb <61048602+Floutch-dtb@users.noreply.github.com>
2021-09-21 11:21:00 +0200

    Update agenda.js

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -264,7 +264,7 @@ jsConstruitAgenda = function(sListeEvent = "") {
 }
 
 
-const fileUrl = './agenda.txt'
+const fileUrl = 'agenda.txt'
 fetch(fileUrl)
   .then(objResultat => objResultat.text())
   .then(sTexte => jsConstruitAgenda(sTexte))
